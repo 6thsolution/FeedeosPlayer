29ad36b44e3a1712b28cfcf06c1f75b5b5dadd70
mehdi sohrabi <mehdok@gmail.com>
2017-09-14 08:37:33 +0430

    Initial Commit

---
+++ FeedeosPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FeedeosPlayer
+//
+//  Created by papyrus on 9/14/17.
+//  Copyright © 2017 SixthSolution. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
